d9e2cc359416e27589a265a88de343e6ec2a51ef
Pascal Schärli <github@pascscha.ch>
2021-12-26 15:02:35 +0100

    Generator for cone animation

---
+++ generators/__init__.py
@@ -0,0 +1,151 @@
+import numpy as np
+from visualization.visualize import Animation
+import colorsys
+
+
+def write_animation(animation, path):
+    animation = animation.reshape(animation.shape[0], -1)
+
+    with open(path, "w+") as f:
+        # Write header
+        f.write("FRAME_ID")
+        for i in range(animation.shape[1]//3):
+            f.write(f",R_{i},G_{i},B_{i}")
+        f.write("\n")
+
+        for frame_id, frame in enumerate(animation):
+            f.write(f"{frame_id}," + ",".join(map(str, frame)) + "\n")
+
+
+def rgb_to_hsv(animation):
+    for frame_idx in range(len(animation)):
+        for pixel_idx in range(len(animation[frame_idx])):
+            r, g, b = animation[frame_idx][pixel_idx]
+            animation[frame_idx][pixel_idx] = colorsys.rgb_to_hsv(r, g, b)
+    return animation
+
+
+def hsv_to_rgb(animation):
+    for frame_idx in range(len(animation)):
+        for pixel_idx in range(len(animation[frame_idx])):
+            h, s, v = animation[frame_idx][pixel_idx]
+            animation[frame_idx][pixel_idx] = colorsys.hsv_to_rgb(h, s, v)
+    return animation
+
+
+class AnimatedFunction:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    @classmethod
+    def _mul_args(cls, args, amount):
+        if isinstance(args, list):
+            return [cls._mul_args(a, amount) for a in args]
+        elif isinstance(args, tuple):
+            return tuple([cls._mul_args(a, amount) for a in args])
+        elif isinstance(args, dict):
+            return {k: cls._mul_args(v, amount) for k, v in args.items()}
+        else:
+            return args * amount
+
+    @classmethod
+    def _add_args(cls, args1, args2):
+        if isinstance(args1, list):
+            assert isinstance(args2, list) and len(args1) == len(args2)
+            return [cls._add_args(a1, a2) for a1, a2 in zip(args1, args2)]
+        elif isinstance(args1, tuple):
+            assert isinstance(args2, tuple) and len(args1) == len(args2)
+            return tuple([cls._add_args(a1, a2) for a1, a2 in zip(args1, args2)])
+        elif isinstance(args1, dict):
+            assert isinstance(args2, dict) and len(args1) == len(args2)
+            return {k1: cls._add_args(v1, args2[k1]) for k1, v1 in args1.items()}
+        else:
+            return args1 + args2
+
+    def __mul__(self, amount):
+        args = self._mul_args(self.args, amount)
+        kwargs = self._mul_args(self.kwargs, amount)
+        return self.__class__(*args, **kwargs)
+
+    def __add__(self, other):
+        args = self._add_args(self.args, other.args)
+        kwargs = self._add_args(self.kwargs, other.kwargs)
+        return self.__class__(*args, **kwargs)
+
+    def get_frame(self, coords, frame):
+        raise NotImplementedError("Please implement this method.")
+
+
+class Cone(AnimatedFunction):
+    def __init__(self, base, height, inner_radius, outer_radius, color):
+        super().__init__(base, height, inner_radius, outer_radius, color)
+        self.base = base
+        self.height = height
+        self.inner_radius = inner_radius
+        self.outer_radius = outer_radius
+        self.color = color
+
+    def get_frame(self, coords, frame):
+        for i, coord in enumerate(coords):
+            vect = coord - self.base
+            r = 1 - vect[2] / self.height
+            if 0 < r < 1:
+                d = (vect[0] ** 2 + vect[1] ** 2) ** 1/2
+                if self.inner_radius * r <= d < self.outer_radius * r:
+                    frame[i] = self.color
+        return frame
+
+class Combiner(AnimatedFunction):
+    def __init__(self, functions):
+        super().__init__(functions)
+        self.functions = functions
+
+    def get_frame(self, coords, frame):
+        for f in self.functions:
+            frame = f.get_frame(coords, frame)
+        return frame
+
+def inter_linear(x):
+    return x
+
+def inter_squared(x):
+    return x**2
+
+def inter_squared_inv(x):
+    return 1 - (1-x)**2
+
+def animate(coords, f1, f2, interval, duration, interpolation=inter_linear):
+    n_frames = int(duration / interval)
+    out = np.zeros((n_frames, len(coords), 3), dtype=np.float32)
+    for i in range(n_frames):
+        prog = interpolation((i+1)/n_frames)
+        f = f1 * (1 - prog) + f2 * prog
+        out[i] = f.get_frame(coords, out[i])
+    return out
+
+def generate(events, path, interval):
+    coords = Animation.load_csv("coords_2021.csv")
+    animations = []
+    for i in range(len(events) - 1):
+        animations.append(
+            animate(
+                coords, 
+                events[i][0],
+                events[i+1][0],
+                interval,
+                events[i+1][1],
+                interpolation=events[i+1][2]                
+            )
+        )
+
+
+    tot_len = sum(len(a) for a in animations)
+    animation = np.empty((tot_len, len(coords), 3), dtype=np.float32)
+    i = 0
+    for a in animations:
+        animation[i:i+len(a)] = a
+        i += len(a)
+
+    animation = (hsv_to_rgb(animation) * 255).astype(np.uint8)
+    write_animation(animation, path)
---
+++ generators/cone.py
@@ -0,0 +1,20 @@
+from generators import *
+
+if __name__ == "__main__":
+    INTERVAL = 33
+
+    GREEN_BRIGHT = (0.28, 1, 1)
+    GREEN_DARK = (0.28, 1, 0.3)
+    RED_BRIGHT = (0, 1, 1)
+
+    init = Combiner([Cone((0, 0, 0), 3, 0.2, 0.6, GREEN_DARK)])
+
+    events = [
+        (init, None, None),
+        (Combiner([Cone((0, 0, -2), 3, 0.2, 0.6, GREEN_BRIGHT)]), 500, inter_squared_inv),
+        (Combiner([Cone((0, 0, 2), 3, 0.2, 0.6, GREEN_DARK)]), 500, inter_squared_inv),
+        (Combiner([Cone((0, 0, 2), -2, 0.2, 0.6, RED_BRIGHT)]), 500, inter_linear),
+        (init, 500, inter_linear),
+    ]
+
+    generate(events, "examples/cones.csv", INTERVAL)
